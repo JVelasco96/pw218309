64492a4fd2ec135f3c1d29bce316b594deeb4b91
xavier_anotonio@hotmail.com <xavier_anotonio@hotmail.com>
2018-11-01 10:00:03 -0700

    Avance del teclado

--- Claculadora/js/calculadora_js.js
+++ Claculadora/js/calculadora_js.js
@@ -1,6 +1,41 @@
-var colorAmarillo = function(x){
-	x.style.background = "yellow";
+window.onload = function(){
+
+	var colorAmarillo = function(){
+		this.style.background = "yellow";
+	}
+	var colorBlanco = function(){
+		this.style.background = "white";
+	}
+
+	var operando1 = document.getElementById("operando1");
+	operando1.addEventListener("focus", colorAmarillo);
+	operando1.addEventListener("focusout", colorBlanco);
+
+	var operador = "";
+	var numeros = function(){
+	var valor = this.value;
+
+		if(operador == ""){ // Operando 1
+			var valorInput = document.getElement("operando1").value;
+			if(valorInput == "0"){
+				document.getElementById("operando1").value = "";
+			
+		}
+		document.getElementById("operando1").value += valor;
+		}else{//Operando 2
+			document.getElementById("operando2").value = "";
+			document.getElementById("operando2").value += valor;
+		}
+
+	}
+
+	var uno = document.getElementById("uno")
+	uno.addEventListener("click", numeros);
+
+
+	
 }
-var colorBlanco = function(x){
-	x.style.background = "white";
-}
+
+
+
+
