19fa739b2a8d6be4d630a74dd86aee6e616896eb
xavier_anotonio@hotmail.com <xavier_anotonio@hotmail.com>
2018-11-08 09:59:50 -0700

    Calculadora electron terminada

---
+++ npmproyecto/main.js
@@ -0,0 +1,33 @@
+// cargar la aplicacion de electron
+const app=require('electron').app;
+
+//crear ventanas del sistema operativo.
+const BrowserWindow = require('electron').BrowserWindow;
+
+//Ruta del sistema de archicos del sistema operativo
+const path = require('path');
+const url = require('url');
+//Otra forma de declarar una constante
+
+//Pantalla principal
+let PantallaPrincipal;
+
+function muestraPantallaPrincipal(){
+	//creamos pantalla vacia
+	PantallaPrincipal = new BrowserWindow({width: 380, height: 420});
+
+	//Cargamos en la pantalla principal el contenido de nuestra pagina
+	PantallaPrincipal.loadURL(
+		url.format({
+			pathname: path.join(__dirname, 'index.html'),
+			protocol: 'file',
+			slashes: 'true'
+		})
+	);
+
+	//Mostramos la pantalla 
+	PantallaPrincipal.show();
+}
+
+app.on('ready', muestraPantallaPrincipal);
+
